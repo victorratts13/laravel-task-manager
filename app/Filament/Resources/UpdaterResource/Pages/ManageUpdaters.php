07e5adb88ed3ecc233bb484fb04b0cf2a48d5b73
Hugo Ratts <118541950+vratts@users.noreply.github.com>
2024-07-22 12:48:18 -0300

    update changelogs and config variables #1

--- app/Filament/Resources/UpdaterResource/Pages/ManageUpdaters.php
+++ app/Filament/Resources/UpdaterResource/Pages/ManageUpdaters.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\UpdaterResource\Pages;
 use App\Filament\Resources\UpdaterResource;
 use Filament\Actions;
 use Filament\Actions\Action;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ManageRecords;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 
 class ManageUpdaters extends ManageRecords
 {
@@ -16,6 +19,34 @@ class ManageUpdaters extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('edit-env')
+                ->requiresConfirmation()
+                ->label('Variáveis de conf.')
+                ->icon('heroicon-c-cog-6-tooth')
+                ->fillForm(function () {
+                    return [
+                        'env' => File::get(base_path('.env'))
+                    ];
+                })
+                ->form([
+                    Toggle::make('migrate')->label('Executar migração de banco de dados após a edição do arquivo'),
+                    Textarea::make('env')
+                        ->label('Edite as variaveis de ambiente do sistema')
+                        ->rows(20)
+                ])
+                ->action(function ($data) {
+                    File::put(base_path('.env'), $data['env']);
+                    if ($data['migrate']) {
+                        Artisan::call('migrate');
+                    }
+
+                    Notification::make()
+                        ->success()
+                        ->title('Sucesso')
+                        ->body('Variáveis de ambiete atualizadas com sucesso!!')
+                        ->send();
+                }),
+
             Action::make('Update')
                 ->icon('heroicon-c-arrow-path')
                 ->color('info')
